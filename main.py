1e8e64ff08f1b23bb6f134ce9ba8a7fde5a32a06
abhishekk962 <abhishekk962@gmail.com>
2023-06-02 11:29:12 +0530

    stop characteristics fixed

--- main.py
+++ main.py
@@ -124,30 +124,71 @@ def stop_char():
     stops = [n[1] for n in data]
     # return str(stops)
     if request.method == "POST":
-        # Form data
-        data = request.form.to_dict()
-        before_int = [True if f"{n}_before" in data else False for n in stop_ids]
-        far_from_int = [True if f"{n}_far" in data else False for n in stop_ids]
-        commercial = [data[n] for n in data if '_comm' in n]
-        transport_hub = [data[n] for n in data if '_tran' in n]
-        bus_bay = [True if f"{n}_busbay" in data else False for n in stop_ids]
+        if 'save' in request.form:
+            # Form data
+            data = request.form.to_dict()
+            before_int = [True if f"{n}_before" in data else False for n in stop_ids]
+            far_from_int = [True if f"{n}_far" in data else False for n in stop_ids]
+            commercial = [data[n] for n in data if '_comm' in n]
+            transport_hub = [data[n] for n in data if '_tran' in n]
+            bus_bay = [True if f"{n}_busbay" in data else False for n in stop_ids]
 
-        # Upload to Database
-        c = conn.cursor()
-        query = f"CREATE TABLE IF NOT EXISTS T_STOPS_INFO (id INT NOT NULL AUTO_INCREMENT,uid VARCHAR(50),Operator TEXT,\
-                Stop_Name TEXT,Stop_Lat FLOAT,Stop_Long FLOAT, Dummy BOOLEAN, Cong_Int BOOLEAN,Before_Int BOOLEAN, \
-                Far_From_Int BOOLEAN, Commercial FLOAT, Transport_Hub FLOAT, Bus_bay BOOLEAN, PRIMARY KEY (id));"
-        c.execute(query)
-        conn.commit()
-
-        c = conn.cursor()
-        for n in range(len(stops)):
-            c.execute(f"UPDATE T_STOPS_INFO SET Before_Int = {before_int[n]}, Far_From_Int = {far_from_int[n]} , Commercial = \
-                      '{commercial[n]}' , Transport_Hub = '{transport_hub[n]}' , Bus_bay = {bus_bay[n]} WHERE id = '{stop_ids[n]}';")
+            # Upload to Database
+            c = conn.cursor()
+            query = f"CREATE TABLE IF NOT EXISTS T_STOPS_INFO (id INT NOT NULL AUTO_INCREMENT,uid VARCHAR(50),Operator TEXT,\
+                    Stop_Name TEXT,Stop_Lat FLOAT,Stop_Long FLOAT, Dummy BOOLEAN, Cong_Int BOOLEAN,Before_Int BOOLEAN, \
+                    Far_From_Int BOOLEAN, Commercial FLOAT, Transport_Hub FLOAT, Bus_bay BOOLEAN, PRIMARY KEY (id));"
+            c.execute(query)
             conn.commit()
-        return render_template('only_stopchar.html',stops=stops, stop_ids=stop_ids, message="Data was Saved")
+
+            c = conn.cursor()
+            for n in range(len(stops)):
+                c.execute(f"UPDATE T_STOPS_INFO SET Before_Int = {before_int[n]}, Far_From_Int = {far_from_int[n]} , Commercial = \
+                        '{commercial[n]}' , Transport_Hub = '{transport_hub[n]}' , Bus_bay = {bus_bay[n]} WHERE id = '{stop_ids[n]}';")
+                conn.commit()
+            return render_template('only_stopchar.html',stops=stops, stop_ids=stop_ids, message="Data was Saved")
+        elif 'getfromdb' in request.form:
+            c = conn.cursor()
+            c.execute(f"SELECT Before_Int,Far_From_Int,Commercial,Transport_Hub,Bus_bay FROM T_STOPS_INFO WHERE id IN {tuple(stop_ids)}")
+            data= c.fetchall()
+            # return str(data)
+            return render_template('only_stopchar.html',stops=stops, stop_ids=stop_ids, message="Data was updated from DB", data=data)
 
     return render_template('only_stopchar.html',stops=stops,stop_ids=stop_ids, message="")
+
+# @app.route('/get-stop-char', methods=['GET', 'POST'])
+# def get_stop_char():
+#     c = conn.cursor()
+#     c.execute(f"SELECT Before_Int,Far_From_Int,Commercial,Transport_Hub,Bus_bay FROM T_ROUTE_INFO AS r INNER JOIN T_STOPS_INFO AS s ON (s.id = r.Stop_id) WHERE r.Operator = '{session['email']}' and r.Route='{session['route']}' ORDER BY r.Stop_Num")
+#     data= c.fetchall()
+#     stop_ids = [n[0] for n in data]
+#     stops = [n[1] for n in data]
+#     # return str(stops)
+#     if request.method == "POST":
+#         # Form data
+#         data = request.form.to_dict()
+#         before_int = [True if f"{n}_before" in data else False for n in stop_ids]
+#         far_from_int = [True if f"{n}_far" in data else False for n in stop_ids]
+#         commercial = [data[n] for n in data if '_comm' in n]
+#         transport_hub = [data[n] for n in data if '_tran' in n]
+#         bus_bay = [True if f"{n}_busbay" in data else False for n in stop_ids]
+
+#         # Upload to Database
+#         c = conn.cursor()
+#         query = f"CREATE TABLE IF NOT EXISTS T_STOPS_INFO (id INT NOT NULL AUTO_INCREMENT,uid VARCHAR(50),Operator TEXT,\
+#                 Stop_Name TEXT,Stop_Lat FLOAT,Stop_Long FLOAT, Dummy BOOLEAN, Cong_Int BOOLEAN,Before_Int BOOLEAN, \
+#                 Far_From_Int BOOLEAN, Commercial FLOAT, Transport_Hub FLOAT, Bus_bay BOOLEAN, PRIMARY KEY (id));"
+#         c.execute(query)
+#         conn.commit()
+
+#         c = conn.cursor()
+#         for n in range(len(stops)):
+#             c.execute(f"UPDATE T_STOPS_INFO SET Before_Int = {before_int[n]}, Far_From_Int = {far_from_int[n]} , Commercial = \
+#                       '{commercial[n]}' , Transport_Hub = '{transport_hub[n]}' , Bus_bay = {bus_bay[n]} WHERE id = '{stop_ids[n]}';")
+#             conn.commit()
+#         return render_template('only_stopchar.html',stops=stops, stop_ids=stop_ids, message="Data was Saved")
+
+#     return render_template('only_stopchar.html',stops=stops,stop_ids=stop_ids, message="")
 
 @app.route('/table', methods=['GET', 'POST'])
 def table_details():
